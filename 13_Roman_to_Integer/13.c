f436eab9eea28fb81dc9ddf895253a96f78d67cb
107061218 <leoshieh0hn@gmail.com>
2021-08-29 17:40:08 +0800

    ad 13

---
+++ 13_Roman_to_Integer/13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int romanToInt(char *)
+
+int main()
+{
+    // printf('%d\n', romanToInt("LVIII"));
+    romanToInt("LVIII")
+    return 0;
+}
+
+int romanToInt(char * s)
+{
+    for(int i = 0; i < strlen(s); i++) {
+        printf("%c ", s[i]);
+    }
+}
