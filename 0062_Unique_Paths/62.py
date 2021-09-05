267823cf3f9337ab0578cf89974185b03c8a6e01
107061218 <leoshieh0hn@gmail.com>
2021-09-05 20:14:54 +0800

    revise my method

--- 0062_Unique_Paths/62.py
+++ 0062_Unique_Paths/62.py
@@ -6,7 +6,18 @@ def fac(i: int) -> int:
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return fac(m + n - 2) // (fac(m - 1) * fac(n - 1))
+        # return fac(m + n - 2) // (fac(m - 1) * fac(n - 1))
+        temp = 1
+        total = m + n - 2
+        larger = m - 1 if m > n else n - 1
+        smaller = n - 1 if larger == m - 1 else m - 1
+        # print(total, larger, smaller)
+        for i in range(larger + 1, total + 1):
+            temp *= i
+            # print(f'i = {i}, temp = {temp}')
+        # print(f'fac smaller = {fac(smaller)}')
+        temp //= fac(smaller)
+        return temp 
 
 a = Solution()
 print(a.uniquePaths(m = 3, n = 7))
