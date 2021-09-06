304c87386e87894b99a172e867e99ef7b2a2e399
107061218 <leoshieh0hn@gmail.com>
2021-09-06 20:18:08 +0800

    add 345

---
+++ 0345_Reverse_Vowels_of_a_String/345.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string reverseVowels(string s)
+    {
+        vector<char> vowels;
+        int idx;
+        for (int i = 0; i < s.length(); i++)
+        {
+            char ch = s[i];
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                vowels.push_back(ch);
+            }
+        }
+        idx = vowels.size() - 1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            char ch = s[i];
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                s[i] = vowels[idx];
+                idx--;
+            }
+        }
+        return s;
+    }
+};
+
+int main()
+{
+    Solution a;
+    string s1 = "hello";
+    string s2 = "leetcode";
+    cout << a.reverseVowels(s1) << "\n"
+         << a.reverseVowels(s2) << "\n";
+    return 0;
+}
