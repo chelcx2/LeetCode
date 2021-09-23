76208b0dad37c57e777b13ce9cef71020c4ed106
107061218 <leoshieh0hn@gmail.com>
2021-09-24 01:01:04 +0800

    add python solution using stack

---
+++ 0020_Valid_Parentheses/20.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mystack = []
+        for p in s:
+            if p in ['(', '[', '{']:
+                mystack.append(p)
+            elif p == ')':
+                try:
+                    assert mystack.pop() == '('
+                except:
+                    return False
+            elif p == ']':
+                try:
+                    assert mystack.pop() == '['
+                except:
+                    return False
+            elif p == '}':
+                try:
+                    assert mystack.pop() == '{'
+                except:
+                    return False
+        return True if not mystack else False
+
+a = Solution()
+print(a.isValid(s = "()"))
+print(a.isValid(s = "()[]{}"))
+print(a.isValid(s = "(]"))
+print(a.isValid(s = "([)]"))
+print(a.isValid(s = "{[]}"))
