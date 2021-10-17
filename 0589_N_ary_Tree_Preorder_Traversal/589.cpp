0956d22548cb6a82ad98bb6e0126f5fac4f49436
107061218 <leoshieh0hn@gmail.com>
2021-10-17 14:46:33 +0800

    add 589 (tree prac)

---
+++ 0589_N_ary_Tree_Preorder_Traversal/589.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+// Definition for a Node.
+using namespace std;
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution
+{
+    vector<int> result;
+
+public:
+    vector<int> preorder(Node *root)
+    {
+        if (root)
+        {
+            result.push_back(root->val);
+            vector<Node *> children = root->children;
+            for (int i = 0; i < children.size(); i++)
+            {
+                // cout << children[i]->val << " ";
+                preorder(children[i]);
+            }
+        }
+        return result;
+    }
+};
