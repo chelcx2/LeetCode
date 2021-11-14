6673cefdc6fe27b42b47525f211e2fd6186d8560
107061218 <leoshieh0hn@gmail.com>
2021-11-15 03:33:06 +0800

    remove misnamed directory

--- 0557_Reverse_Words_in_a_String III/557.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        return ' '.join([sub[::-1] for sub in s.split(' ')])
-a = Solution()
-print(a.reverseWords(s = "Let's take LeetCode contest"))
-print(a.reverseWords(s = "God Ding"))
