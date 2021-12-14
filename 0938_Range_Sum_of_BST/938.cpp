3215a5e06d11f0ae8e83ef7ba128c3a36d164f80
107061218 <leoshieh0hn@gmail.com>
2021-12-14 23:23:49 +0800

    add 938 (BST traversal)

---
+++ 0938_Range_Sum_of_BST/938.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+int ans;
+void visit(TreeNode* root, int low, int high) {
+    if (root->val >= low && root->val <= high) {
+        ans += root->val;
+    }
+    if (root->left)
+        visit(root->left, low, high);
+    if (root->right)
+        visit(root->right, low, high);
+}
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        ans = 0;
+        visit(root, low, high);
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
