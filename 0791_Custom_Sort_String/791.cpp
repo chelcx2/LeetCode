b3e0f8d6ff3fe2c5ca48bde10239df8999de6665
107061218 <leoshieh0hn@gmail.com>
2021-12-23 13:53:42 +0800

    add 791 with runtime=0ms

---
+++ 0791_Custom_Sort_String/791.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string ans = "";
+        for (const auto ch: order) {
+            int cnt = count(s.begin(), s.end(), ch);
+            for (int i = 0; i < cnt; i++)
+                ans += ch;
+            s.erase(remove(s.begin(), s.end(), ch), s.end());
+        }
+        return ans + s;
+    }
+};
+
+int main() {
+    string ord1 = "cba", s1 = "abcd";
+    string ord2 = "cbafg", s2 = "abcd";
+    Solution a;
+    cout << a.customSortString(ord1, s1) << "\n" << a.customSortString(ord2, s2);
+    // cout << a.customSortString(ord2, s2);
+    return 0;
+}
