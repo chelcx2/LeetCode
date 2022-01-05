7fdcc8d83279fc6b0f6002bfe337bb20f3e8a2f1
107061218 <leoshieh0hn@gmail.com>
2022-01-05 22:43:18 +0800

    add 705

---
+++ 0705_Design_HashSet/705.py
@@ -0,0 +1,23 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.S = set()
+
+    def add(self, key: int) -> None:
+        self.S.add(key)
+
+    def remove(self, key: int) -> None:
+        try:
+            self.S.remove(key)
+        except:
+            pass
+
+    def contains(self, key: int) -> bool:
+        return key in self.S
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
