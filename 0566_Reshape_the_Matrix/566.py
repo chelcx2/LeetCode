99f0f69e2822bd1d7d461b3cce375468b5cd52c6
107061218 <leoshieh0hn@gmail.com>
2022-01-30 20:42:41 +0800

    add 566 | Faster than 91.73% | Less mem than 95.48%

---
+++ 0566_Reshape_the_Matrix/566.py
@@ -0,0 +1,25 @@
+class Solution:
+    def matrixReshape(self, mat: list[list[int]], r: int, c: int) -> list[list[int]]:
+        if r * c != len(mat) * len(mat[0]):
+            return mat
+        ans = []
+        for _ in range(r):
+            temp = []
+            for _ in range(c):
+                temp.append(0)
+            ans.append(temp)
+        # print(temp)
+        row, col = 0, 0
+        for line in mat:
+            for element in line:
+                ans[row][col] = element
+                if col == c - 1:
+                    row += 1
+                    col = 0
+                else:
+                    col += 1
+        return ans
+
+a = Solution()
+print(a.matrixReshape(mat = [[1,2],[3,4]], r = 1, c = 4))
+print(a.matrixReshape(mat = [[1,2],[3,4]], r = 2, c = 4))
