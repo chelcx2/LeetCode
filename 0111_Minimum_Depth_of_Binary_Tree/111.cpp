d22d2e7e35200403c232d6703b9e965af76199fe
107061218 <leoshieh0hn@gmail.com>
2022-02-09 23:26:05 +0800

    add 111 (tree practice)

---
+++ 0111_Minimum_Depth_of_Binary_Tree/111.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<int> depth;
+
+class Solution {
+public:
+    int visit(TreeNode* t, int prev) {
+        if (!t->left && !t->right)
+            depth.push_back(prev + 1);
+        if (t->left)
+            visit(t->left, prev + 1);
+        if (t->right)
+            visit(t->right, prev + 1);
+        return prev + 1;
+    }
+    int minDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        depth.clear();
+        visit(root, 0);
+        // for (const int i: depth)
+        //     printf("%d ", i);
+        return *min_element(depth.begin(), depth.end());
+    }
+};
+
+int main() {
+    return 0;
+}
