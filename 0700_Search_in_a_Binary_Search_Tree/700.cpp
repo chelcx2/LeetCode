1ae571f2d228760c152a766711c6cfcfc3cb4594
107061218 <leoshieh0hn@gmail.com>
2022-02-18 11:44:13 +0800

    add 700 (tree practice)

---
+++ 0700_Search_in_a_Binary_Search_Tree/700.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while (1) {
+            if (root->val == val)
+                return root;
+            if (val > root->val && root->right)
+                root = root->right;
+            else if (val < root->val && root->left)
+                root = root->left;
+            else
+                return nullptr;
+        }
+    }
+};
