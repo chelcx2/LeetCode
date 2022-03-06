750b0621d7794a70c06b4fd3acc1e7a5ebbe4a59
107061218 <leoshieh0hn@gmail.com>
2022-03-06 14:55:58 +0800

    add 2108

---
+++ 2108_Find_First_Palindromic_String_in_the_Array/2108.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstPalindrome(self, words: list[str]) -> str:
+        for word in words:
+            if word == word[::-1]:
+                return word
+        return ''
+
+a = Solution()
+print(a.firstPalindrome(words = ["abc","car","ada","racecar","cool"]))
